9797114e67aaec0e030b61855067698c2e1be003
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-12 09:18:59 +0200

    main structure

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 "use strict";
 
 console.log(">> Ready :) <<");
+
+const numberInput = document.querySelector(".js-number-input");
+const buttonInput = document.querySelector(".js-button-input");
+const clueInput = document.querySelector(".js-clue-input");
+const accInput = document.querySelector(".js-acc-input");
