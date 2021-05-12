655887f465435fcd9d19f37d27e2585846503666
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-12 11:01:47 +0200

    change acc

--- js/main.js
+++ js/main.js
@@ -2,45 +2,57 @@
 
 console.log(">> Ready :) <<");
 
+// Variables globales (campos generales)
+// Variable numero usuaria
 const numberInput = document.querySelector(".js-number-input");
+// Variable botón prueba
 const buttonInput = document.querySelector(".js-button-input");
-const clueInput = document.querySelector(".js-clue-input");
-const accInput = document.querySelector(".js-acc-input");
+// Variable valor input Pista
+const clueText = document.querySelector(".js-clue");
+/*clueInput.innerHTML = "Pista: Escribe un número y dale a Prueba";*/
+// Variable valor input Acumulador
+const accText = document.querySelector(".js-acc");
 
+// Función genera nº aleatorio y pintarlo en consola
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(getRandomNumber(100));
+console.log(`El número aleatorio es ${getRandomNumber(100)}`);
 
+// Función recoger numero usuaria y pintarlo en consola
 function getUserNumber() {
-  const numberUser = numberInput.value;
-  return console.log(numberUser);
+  const numberUser = parseInt(numberInput.value);
+  console.log(`El número escrito por la usuaria es ${numberUser}`);
+  return numberUser;
 }
+console.log(getUserNumber());
 
+// Función comparar número usuaria con número aleatorio para pintar pista en input
 function compareNumbers() {
   if (getUserNumber > getRandomNumber) {
-    clueInput.innerHTML = "Demasiado alto";
+    clueText.innerHTML = "Pista: Demasiado alto";
   } else if (getUserNumber < getRandomNumber) {
-    clueInput.innerHTML = "Demasiado bajo";
-  } else if (getUserNumber > 100 || getUserNumber < 0) {
-    clueInput.innerHTML = "El número debe estar entre 1 y 100";
-  } else if (getUserNumber === getRandomNumber) {
-    clueInput.innerHTML = "Has ganado campeona!!!";
+    clueText.innerHTML = "Pista: Demasiado bajo";
+  } else if (getUserNumber > 100 || b < 0) {
+    clueText.innerHTML = "Pista: El número debe estar entre 1 y 100";
+  } /*else if (getUserNumber === "") {
+    clueText.innerHTML = "Pista: El número debe estar entre 1 y 100";
+  }*/ else if (getUserNumber === getRandomNumber) {
+    clueText.innerHTML = "Has ganado campeona!!!";
   }
-}
-function accNumbers() {
-  const accValue = accInput.value;
-  let acc = 0;
-  if (getUserNumber) {
-    acc += 1;
-  }
-  return (accValue.innerHTML = acc);
 }
 
+// Función acumulador de intentos (sin bucles ni arrays)
+function accNumbers() {
+  accText.innerHTML = parseInt(accText.innerHTML) + 1;
+}
+
+// Función manejadora que recoje el proceso completo
 function handlerNumber() {
   getUserNumber();
   compareNumbers();
   accNumbers();
 }
 
+// Evento que tiene lugar cuando la usuaria hace click en el botón
 buttonInput.addEventListener("click", handlerNumber);
