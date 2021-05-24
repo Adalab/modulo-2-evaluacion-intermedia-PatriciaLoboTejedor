3547bc173f05527ef917a0959f43ddc588b9656a
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-24 13:40:43 +0200

    fit issue code review

--- js/main.js
+++ js/main.js
@@ -1,44 +1,52 @@
 "use strict";
 
-console.log(">> Ready :) <<");
-
-// Variables globales (campos generales)
-// Variable numero usuaria
+// Constantes globales (campos generales)
+// Constante numero usuaria
 const numberInput = document.querySelector(".js-number-input");
-console.log(numberInput);
-// Variable botón prueba
+// Constante botón prueba
 const buttonInput = document.querySelector(".js-button-input");
-// Variable valor input Pista
+// Constante valor input Pista
 const clueText = document.querySelector(".js-clue");
-/*clueInput.innerHTML = "Pista: Escribe un número y dale a Prueba";*/
-// Variable valor input Acumulador
+// Constante valor input Acumulador
 const accText = document.querySelector(".js-acc");
+// Constante formulario
+const form = document.querySelector(".js-form");
+// Constante botón prueba
+const buttonReset = document.querySelector(".js-button-reset");
 
-// Función genera nº aleatorio y pintarlo en consola
+// Función que genera nº aleatorio y lo pinta en consola
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const randoNumber = getRandomNumber(100);
-console.log(`El número aleatorio es ${randoNumber}`);
+
+// Ejecución automática que genera el número aleatorio de 0-100
+let randomNumber = getRandomNumber(100);
+console.log(`El número aleatorio es ${randomNumber}`);
 
 // Función recoger numero usuaria y pintarlo en consola
 function getUserNumber() {
   const numberUser = parseInt(numberInput.value);
-  console.log(numberUser);
   return numberUser;
 }
-console.log(`El número escrito por la usuaria es ${getUserNumber()}`);
+
+// Función que minimiza el uso de innerhtml de manera repetitiva
+function clueTextHtml(clue) {
+  clueText.innerHTML = clue;
+}
 
 // Función comparar número usuaria con número aleatorio para pintar pista en input
 function compareNumbers() {
-  if (getUserNumber() > 100 || getUserNumber() < 0) {
-    clueText.innerHTML = "Pista: El número debe estar entre 1 y 100";
-  } else if (getUserNumber() > randoNumber) {
-    clueText.innerHTML = "Pista: Demasiado alto";
-  } else if (getUserNumber() < randoNumber) {
-    clueText.innerHTML = "Pista: Demasiado bajo";
-  } else if (getUserNumber() === randoNumber) {
-    clueText.innerHTML = "Has ganado campeona!!!";
+  const userNumberFunction = getUserNumber();
+  if (userNumberFunction !== userNumberFunction) {
+    clueTextHtml("Debes introducir un número");
+  } else if (userNumberFunction > 100 || userNumberFunction < 0) {
+    clueTextHtml("Pista: El número debe estar entre 1 y 100");
+  } else if (userNumberFunction > randomNumber) {
+    clueTextHtml("Pista: Demasiado alto");
+  } else if (userNumberFunction < randomNumber) {
+    clueTextHtml("Pista: Demasiado bajo");
+  } else if (userNumberFunction === randomNumber) {
+    clueTextHtml("Has ganado campeona!!!");
   }
 }
 
@@ -47,12 +55,30 @@ function accNumbers() {
   accText.innerHTML = parseInt(accText.innerHTML) + 1;
 }
 
-// Función manejadora que recoje el proceso completo
+// Función manejadora que recoje el proceso de juego completo
 function handlerNumber() {
   getUserNumber();
   compareNumbers();
   accNumbers();
 }
 
+// Función que resetea el juego
+function handlerReset() {
+  // Vacío todos los campos, incluida la consola
+  clueTextHtml("Pista: Escribe el número y dale a Prueba");
+  accText.innerHTML = parseInt(0);
+  numberInput.value = "";
+  console.clear();
+  // Genero nuevo número aleatorio y lo pinto en la consola
+  randomNumber = getRandomNumber(100);
+  console.log(`El número aleatorio es ${randomNumber}`);
+}
+
 // Evento que tiene lugar cuando la usuaria hace click en el botón
 buttonInput.addEventListener("click", handlerNumber);
+
+// Evento que tiene lugar cuando la usuaria pulsa enter del teclado
+form.addEventListener("submit", handlerNumber);
+
+// Botón de reset para vaciar el juego
+buttonReset.addEventListener("click", handlerReset);
